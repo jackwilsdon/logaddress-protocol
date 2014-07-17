12644143c2fd59fd5a79a17647650cf7a3f9a231
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-07-17 21:47:52 +0100

    Remove JS code

--- python/server.py
+++ python/server.py
@@ -41,29 +41,3 @@ while True:
 		value = 'Invalid packet [{0}]'.format(e)
 
 	print(value)
-
-"""
-server.on('listening', function() {
-	console.log('Server listening on ' + getAddress(server.address()));
-});
-
-server.on('message', function(message, remote) {
-
-	var value = 'Invalid packet';
-
-	try {
-		var packet = LogPacket.parse(new Buffer(message), (secret.length > 0) ? secret : undefined);
-		value = packet.toString();
-	} catch (e) {
-		value = 'Invalid packet [' + e.message + ']';
-	}
-
-	console.log(getAddress(remote) + ' - ' + value);
-});
-
-server.on('close', function() {
-	console.log('Closed server');
-});
-
-server.bind(1337);
-"""
